1bd23c92ece8a147d58e8652d8de3ca5cc52722a
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2026-02-12 15:16:43 +1100

    Add files via upload

--- MathAttack.cpp
+++ MathAttack.cpp
@@ -10,6 +10,7 @@
 //run:     ./MathAttack
 // raylib uses float for most numbers, and so use 2.0f to convert int to float. Note that 2.0 will be a double
 
+// Enemy blocks with numbers
 // Make level editor
 
 
@@ -87,18 +88,11 @@ int Char1[64] = {1,18,23,23,23,23,18,18,1,18,18,23,23,23,18,18,14,16,16,16,16,16
 int CharBlock[576] = {19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19};
 int CharEnemy1[64] = {5,0,0,17,0,0,0,5,0,5,0,0,17,0,5,0,5,0,11,17,0,11,0,5,0,5,5,5,5,5,5,0,5,5,5,5,5,5,5,5,5,5,24,33,24,33,5,5,5,5,5,5,5,5,5,5,0,5,5,5,5,5,5,0};
 
-int Grid[100][100]; // Grid is not needed just remove all blocks with same value
-int gridsize = 16;
-int gridwidth = 50;
-float griddensity = 0.35f;
 
-int herox = gridsize-1;
-int heroy = gridsize-1;
+int herox = 20;
+int heroy = 20;
 
-int gridtoxy(int gridloc)
-{
-     return gridloc*gridwidth+(gridwidth-24)/2;
-}
+int shootnumber = 1;
 
 void drawCharfromArray(int previewx, int previewy, int psize, int bitwidth, int myarray[])
      {
@@ -140,115 +134,46 @@ Enemy::Enemy(int startx, int starty, int number) // constructor
 int Enemy::spawn()
 {
      int count = 0;
-     x = GetRandomValue(0,gridsize-6); // 0 to including gridsize-1
-     y = GetRandomValue(0,gridsize-6); // 0 to including gridsize-1
-     while (Grid[y][x] != 0 and count < 1000)
-     {
-       x = GetRandomValue(0,gridsize-6); // 0 to including gridsize-1
-       y = GetRandomValue(0,gridsize-6); // 0 to including gridsize-1 
-       count++;   
-     }
-     Grid[y][x] = 10;
      return 0;
 }
 
 int Enemy::move()
 {
-     if (x < herox and Grid[y][x+1]  == 0) {Grid[y][x] = 0; x++; Grid[y][x] = 10; return 0; }
-     if (x > herox and Grid[y][x-1]  == 0)
-     {
-          Grid[y][x] = 0;
-          x--;
-          Grid[y][x] = 10;
-          return 0; 
-     }
-     if (y < heroy and Grid[y+1][x]  == 0)
-     { 
-          Grid[y][x] = 0;
-          y++;
-          Grid[y][x] = 10;
-          return 0; 
-     }
-     if (y > heroy and Grid[y-1][x]  == 0)
-     {
-          Grid[y][x] = 0;
-          y--;
-          Grid[y][x] = 10;
-          return 0; 
-     }
+    y = y + 1;
     return 0;
 }
 
 int Enemy::draw()
 {
-  drawCharfromArray(gridtoxy(x), gridtoxy(y), 3,8, CharEnemy1);
+  drawCharfromArray(x, y, 3,8, CharEnemy1);
+  DrawText(to_string(attacknumber).c_str(),x+8,y,40, WHITE);
   return 0;
 }
 vector <Enemy> Enemies;
 
-int moveenemy()
+int moveenemies()
 {
   for (int i=0;i<Enemies.size();i++)
         Enemies[i].move();
   return 0;
 }
 
-void fillgrid(float density)
-{
-     for (int i = 0; i < gridsize; i++)
-       for (int j = 0; j < gridsize; j++)
-       {
-          if (GetRandomValue(0,100)/100.0f < density  and i+j != 2*gridsize-2) 
-          {
-              Grid[j][i] = 1;
-          }
-       }
-}
-
-void cleargrid()
-{
-     for (int i = 0; i < gridsize; i++)
-       for (int j = 0; j < gridsize; j++)
-              Grid[j][i] = 0;
-}
-
-void drawgrid()
-{
-     for (int i = 0; i < gridsize; i++)
-       for (int j = 0; j < gridsize; j++)
-       {
-            if ( Grid[j][i] == 1 )  drawCharfromArray(i*gridwidth, j*gridwidth, 2,24, CharBlock); 
-            if ( Grid[j][i] == 10 ) drawCharfromArray(gridtoxy(i), gridtoxy(j), 3,8, CharEnemy1);
-       }
-}
-
-int pushblock(int x, int y, int dx, int dy)
-{
-   if (x+2*dx >= 0 and y+2*dy >=0 and x+2*dx < gridsize and  y+2*dy < gridsize) 
-    if (Grid[y+dy][x+dx] == 1)
-    {
-      if  (Grid[y+2*dy][x+2*dx] == 0 )
-      {
-       Grid[y+2*dy][x+2*dx]  = 1;
-       Grid[y+dy][x+dx]  = 0; 
-       return 0;
-      }
-      else return -1;
-    }
-   if (x+dx >= 0 and y+dy >=0 and x+dx < gridsize and  y+dy < gridsize)
-    {
-     if (Grid[y+dy][x+dx] == 1) return -1;
-         else return 0;
-    } 
-   return 0;
-}
 
 int createnemies()
 {
-  for (int i=0; i< 1; i++)
-    {  Enemy Entmp(1,1,1);
+  for (int i=0; i< 9; i++)
+    {  Enemy Entmp(screenWidth/2-30,i*40,GetRandomValue(0,9));
        Entmp.spawn();
        Enemies.push_back(Entmp);
+    }
+  return 0;
+}
+
+int drawnemies()
+{
+  for (int i=0; i< Enemies.size(); i++)
+    {  
+       Enemies[i].draw();
     }
   return 0;
 }
@@ -261,36 +186,31 @@ int ReadKeys()
              c++;
         }
    if (IsKeyPressed(KEY_RIGHT))
-        {    
-             if (pushblock(herox, heroy, 1, 0) >= 0 and herox < gridsize -1)
-             {
+        {         
               herox++;
-             }
-             moveenemy();
+              shootnumber++;
+              if (shootnumber > 9) shootnumber = 0;
         }
    if (IsKeyPressed(KEY_UP))
         {
-             if (pushblock(herox, heroy, 0, -1) >= 0 and heroy > 0)
-             { 
+
               heroy--;
-             }
-             moveenemy();
+
+
         }
     if (IsKeyPressed(KEY_LEFT))
         {
-             if (pushblock(herox, heroy, -1, 0) >= 0 and herox > 0)
-             {           
+      
               herox--;
-             }
-             moveenemy();
+              shootnumber--;
+              if (shootnumber < 0) shootnumber = 9;
+
         }
     if (IsKeyPressed(KEY_DOWN))
         {
-             if (pushblock(herox, heroy, 0, 1) >= 0 and heroy < gridsize -1)
-             {
+
               heroy++;
-             }
-             moveenemy();
+
         }
         if (IsKeyPressed(KEY_ONE))
         {
@@ -305,26 +225,32 @@ int ReadKeys()
 
 int main() {
     InitWindow(screenWidth, screenHeight, "Math Attack!"); // RNG seed is set randomly in InitWindow !!
-
+   
+    float moveInterval = 0.02f; // 900 ms 
+    float moveTimer = 0.0f;
     Vector2 MousePos;
     SetTargetFPS(60);
-    cleargrid();
-    fillgrid(griddensity);
     createnemies(); // RNG seed is set randomly in InitWindow !!
-    Enemy Enday(1,1,1);
+    Enemy Enday(1,1,GetRandomValue(0,9));
+    Enemy Enday2(100,100,GetRandomValue(0,9));
     while (!WindowShouldClose()) 
     {
         ReadKeys();
+        float dt = GetFrameTime(); // seconds since last frame 
+        moveTimer += dt; 
+        if (moveTimer >= moveInterval) 
+          { moveenemies(); 
+            moveTimer = 0.0f; // reset timer 
+          }
 
         BeginDrawing();         // these two lines MUST go first when drawing
         ClearBackground(BLACK); // these two lines MUST go first when drawing
 
-        drawgrid(); 
         DrawRectangleLines(0,0,screenWidth,screenHeight,YELLOW);
         MousePos = GetMousePosition();
-        DrawText("<Space> - Save",800,200,20, WHITE);
-        drawCharfromArray(gridtoxy(herox), gridtoxy(heroy), 3,8, Char1);
-        Enday.draw();
+        DrawText(to_string(shootnumber).c_str(),screenWidth/2-40,screenHeight-100,80, WHITE);
+        drawCharfromArray(herox, heroy, 3,8, Char1);
+        drawnemies();
         EndDrawing();
     }
 
